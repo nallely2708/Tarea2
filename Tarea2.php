de45735eb8c11168dbe7d71b328b7653fd9f4da0
nallely2708 <nalle2708@hotmail.com>
2013-09-17 23:13:13 -0500

    Convertidor de dolares a pesos.

---
+++ Tarea2.php
@@ -0,0 +1,13 @@
+<html>
+   <head></head>
+   <body>
+        <?php
+             $dolares = 10;
+             $tasa = 15.5;
+             $pesos = $dolares * $tasa ;
+             echo $pesos ;
+            
+
+        ?>
+    </body>
+</html>
